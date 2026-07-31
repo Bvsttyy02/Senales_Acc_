02d05864a259b724cf6631503e944da5c7785e64
Bvsttyy02 <williamsabate2006@gmail.com>
2026-07-31 01:55:57 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ import requests
 TOKEN = os.environ["TELEGRAM_TOKEN"]
 CHAT_ID = os.environ["TELEGRAM_CHAT_ID"]
 
-requests.post(
+respuesta = requests.post(
     f"https://api.telegram.org/bot{TOKEN}/sendMessage",
-    data={"chat_id": CHAT_ID, "text": "✅ Prueba desde GitHub Actions — si ves esto, funciona."}
+    data={"chat_id": CHAT_ID, "text": "✅ Prueba 2 desde GitHub Actions"}
 )
+
+print("Código:", respuesta.status_code)
+print("Respuesta:", respuesta.text)
