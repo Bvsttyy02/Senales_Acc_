1130b447ec0324676e2b0b7647b60665bce1c971
Bvsttyy02 <williamsabate2006@gmail.com>
2026-08-01 00:50:22 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import os
 import requests
 
-TOKEN = os.environ["8994820850:AAEMSgs4lwT0PEAf9VV8WgaLxZehcBTp9-4"]
-CHAT_ID = os.environ["6420362893"]
+TOKEN = os.environ["TELEGRAM_TOKEN"]
+CHAT_ID = os.environ["TELEGRAM_CHAT_ID"]
+
+print("Largo del TOKEN:", len(TOKEN))
+print("Contiene ':':", ":" in TOKEN)
+print("Primeros 3 caracteres:", repr(TOKEN[:3]))
+print("Últimos 3 caracteres:", repr(TOKEN[-3:]))
+print("Largo del CHAT_ID:", len(CHAT_ID))
 
 respuesta = requests.post(
     f"https://api.telegram.org/bot{TOKEN}/sendMessage",
-    data={"chat_id": CHAT_ID, "text": "✅ Prueba 2 desde GitHub Actions"}
+    data={"chat_id": CHAT_ID, "text": "✅ Prueba 4"}
 )
-
 print("Código:", respuesta.status_code)
 print("Respuesta:", respuesta.text)
